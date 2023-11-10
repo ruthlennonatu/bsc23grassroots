2a0cf74f0f16a57958e7f746480fd48a96954e24
L00163597 <L00163597@atu.ie>
2023-11-10 20:55:35 +0000

    #110 - Ensuring Selenium tests only run for this job

---
+++ Source/grassroots/tests/unitTests/test_basic_test.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+
+class MyTest(TestCase):
+    def test_example(self):
+        # Write your test logic here
+        self.assertEqual(2 + 2, 4)  # Example assertion
