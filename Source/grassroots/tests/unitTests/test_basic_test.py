18766e881c64e64a559e9a46695a8efaf95fc895
L00163597 <L00163597@atu.ie>
2023-11-17 14:17:16 +0000

    Removing tests folder

--- Source/grassroots/tests/unitTests/test_basic_test.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class MyTest(TestCase):
-    def test_example(self):
-        # Write your test logic here
-        self.assertEqual(2 + 2, 4)  # Example assertion
