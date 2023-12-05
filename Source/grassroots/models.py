ed4f24f77d227e3c685894b94e2581d6a6a6d9f7
julieADCL <l00161565@atu.ie>
2023-12-05 23:41:36 +0000

    Updated Database
    Signed-off-by: julieADCL <l00161565@atu.ie>

--- Source/grassroots/models.py
+++ Source/grassroots/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+)
+from django.utils.translation import gettext_lazy as _
+
 
 ########################################################################################################################
 # THE CODE BETWEEN THE HASHES IS NOT BEING USED, BUT IS KEPT FOR REFERENCE, THE CODE BELLOW THIS BLOCK IS THE CURRENT CODE
@@ -59,13 +63,15 @@ class UserManager(BaseUserManager):
         :param extra_fields: Extra fields to be saved with the user.
         :return: The created user object.
         """
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+
+        def create_user(self, email, password=None, **extra_fields):
+            if not email:
+                raise ValueError('The Email field must be set')
+            email = self.normalize_email(email)
+            user = self.model(email=email, **extra_fields)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         """
@@ -78,12 +84,10 @@ class UserManager(BaseUserManager):
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
+        if not extra_fields.get('is_staff'):
             raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
+        if not extra_fields.get('is_superuser'):
             raise ValueError('Superuser must have is_superuser=True.')
-
         return self.create_user(email, password, **extra_fields)
 
 
@@ -98,10 +102,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255)
-    address = models.TextField()
+    phone = models.CharField(max_length=255, blank=True)
+    address = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        related_name="%(app_label)s_%(class)s_groups",
+        related_query_name="%(app_label)s_%(class)ss",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="%(app_label)s_%(class)s_user_permissions",
+        related_query_name="%(app_label)s_%(class)ss",
+    )
 
     objects = UserManager()
 
