60b2b11e13704fbe2c781365f09bdadff13d30d1
julieADCL <l00161565@atu.ie>
2023-12-06 01:38:40 +0000

    form
    Signed-off-by: julieADCL <l00161565@atu.ie>

--- Source/grassroots/forms.py
+++ Source/grassroots/forms.py
@@ -1,11 +1,21 @@
 from django import forms
-from .models import Person  # Adjust the import according to your project structure
+from .models import User, Application, Grant, SpecialAward
 
-'''Classes to connect the forms in the templates to the database models'''
-class PersonForm(forms.ModelForm):
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
-        model = Person
-        fields = ['userID', 'username', 'password', 'first_name', 'last_name', 'phone', 'address']
-        widgets = {
-            'password': forms.PasswordInput(),  # This makes the password field render as a password input
-        }
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'phone', 'address', 'password']
+
+    def save(self, commit=True):
+        # Ensure that the email provided is unique in the system
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise forms.ValidationError('A user with that email already exists.')
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
