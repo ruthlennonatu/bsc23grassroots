a83e7c1af012917ee0d98f70fb338dd0cbf2b646
L00163597 <L00163597@atu.ie>
2023-12-07 21:16:38 +0000

    #136 - Created the Batman class

---
+++ Source/grassroots/Batman.py
@@ -0,0 +1,11 @@
+class Batman:
+
+    @staticmethod
+    def serve_justice() -> bool:
+        bad_guy_in_jail = False
+        batman_exists = True
+
+        if batman_exists:
+            bad_guy_in_jail = True
+
+        return bad_guy_in_jail
