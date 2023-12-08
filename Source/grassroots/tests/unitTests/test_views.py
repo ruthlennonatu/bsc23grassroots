e60a05baa101fa1efef73b38aa456c76ef127a4d
Faran Ahmad Khan (L00179451) <147256652+farankhanatu@users.noreply.github.com>
2023-12-08 17:23:57 +0000

    Create test_views.py

---
+++ Source/grassroots/tests/unitTests/test_views.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+# unit test for "register" view in the Source/grassroots/views.py
+# "register" view is supposed to redirect the user to "home" page upon successful login
+class RegisterViewTest(TestCase):
+    # specifically deals with POST request
+    def test_register_view_post_request(self):
+        # arrange the data for unit test
+        data = {
+            'email': 'testuser@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone': '1234567890',
+            'address': '123 Test St',
+            'password': 'password123',
+        }
+        # create a POST request to "register" view
+        response = self.client.post(reverse('register'), data)
+
+        # check if the user is redirected to home page upon successful login
+        self.assertRedirects(response, reverse('home'))
