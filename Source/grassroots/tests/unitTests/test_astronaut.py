f23ac8bc8686eea66b7965fbfd066bc1c7b4516f
Matthew McDaid <L00160463@atu.ie>
2023-12-21 20:39:09 +0000

    Adding Sample Astronaut class as proof of unit tests issue: #167

--- Source/grassroots/tests/unitTests/test_astronaut.py
+++ Source/grassroots/tests/unitTests/test_astronaut.py
@@ -1,21 +1,19 @@
 from django.test import TestCase
 
-
-def launch_mission() -> bool:
-    mission_successful = False
-    spacecraft_ready = True
-
-    if spacecraft_ready:
-        mission_successful = True
-
-    return mission_successful
-
-
-def send_message_to_earth() -> str:
-    return "Message from space: All systems go!"
-
-
 class Astronaut:
     def __init__(self):
         pass
+
+    def launch_mission(self) -> bool:
+        mission_successful = False
+        spacecraft_ready = True
+
+        if spacecraft_ready:
+            mission_successful = True
+
+        return mission_successful
+
+    def send_message_to_earth(self) -> str:
+        return "Message from space: All systems go!"
+
 
