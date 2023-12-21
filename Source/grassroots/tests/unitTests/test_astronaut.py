1cdf782108a0f730c1a247eaf01dedb090660831
Matthew McDaid <L00160463@atu.ie>
2023-12-21 20:42:51 +0000

    Adding TestAstronaut class as proof of unit tests issue: #167

--- Source/grassroots/tests/unitTests/test_astronaut.py
+++ Source/grassroots/tests/unitTests/test_astronaut.py
@@ -16,4 +16,9 @@ class Astronaut:
     def send_message_to_earth(self) -> str:
         return "Message from space: All systems go!"
 
+class TestAstronaut(TestCase):
+    def test_launch_mission(self):
+        astronaut = Astronaut()
+        self.assertTrue(astronaut.launch_mission())
+
 
